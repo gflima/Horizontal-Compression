4a54bb9675f71cea46c3177f2b7e47cb751f4183
Guilherme F. Lima <guilhermeflima@gmail.com>
2026-02-02 15:46:22 -0300

    nd: Update

--- nd.lean
+++ nd.lean
@@ -3,7 +3,7 @@
 inductive Formula where
 | atom (n : Nat)
 | imp (A B : Formula)
-deriving BEq, ReflBEq, LawfulBEq, DecidableEq, Inhabited
+deriving BEq, ReflBEq, LawfulBEq, DecidableEq
 
 instance : OfNat Formula n where
   ofNat := Formula.atom n
@@ -32,7 +32,7 @@ abbrev Context := List Formula
 
 namespace List
 
-@[simp, grind =] def delete [BEq α] [ReflBEq α] (A : α) :
+@[simp] def delete [BEq α] [ReflBEq α] (A : α) :
     (l : List α) → List α
   | [] => []
   | x :: xs => match x == A with
@@ -57,7 +57,7 @@ inductive Derivation : Context → Formula → Prop where
 | hypo (A : Formula) :
     Derivation [A] A
 
-| impI (d : Derivation G B) :
+| impI {A : Formula} (d : Derivation G B) :
     A ∈ G → H = G/A → Derivation H (A⊃B)
 
 | impE (d₁ : Derivation G₁ A) (d₂ : Derivation G₂ (A⊃B)) :
@@ -81,8 +81,6 @@ example : [] ⊢ (0⊃1)⊃(1⊃2)⊃(0⊃2) := by
   · app impE (hypo 0) (hypo (0⊃1))
   · app hypo
 
--- Basic theorems
-
 theorem MP (d₁ : G₁ ⊢ A) (d₂ : G₂ ⊢ A⊃B) : G₁ ++ G₂ ⊢ B := by
   app impE d₁ d₂
 
@@ -96,8 +94,120 @@ theorem deduct_head (d : A :: G ⊢ B) : G/A ⊢ A⊃B := by
   suffices (A :: G)/A ⊢ A⊃B by rw [←List.delete_head]; trivial
   app deduct (A := A) d
 
--- Proofs
+-- Tree --------------------------------------------------------------------
 
-def Proof (A : Formula) := Derivation [] A
+inductive Tree : Context → Formula → Type where
+
+| hnode (A : Formula)
+    : Tree [A] A
+
+| inode (A : Formula) {B : Formula} (t : Tree G B)
+    : G.elem A → Tree (G/A) (A⊃B)
+
+| enode {A B : Formula} (t₁ : Tree G₁ A) (t₂ : Tree G₂ (A⊃B))
+    : Tree (G₁ ++ G₂) B
+
+deriving Repr
+
+export Tree (hnode inode enode)
+
+-- Gets the root context of the tree.
+def Tree.context : Tree G A → Context
+| hnode a => [a]
+| @inode G a _ _ _ => G/a
+| @enode G₁ G₂ _ _ _ _ => G₁ ++ G₂
+
+-- Gets the root formula of tree.
+def Tree.formula : Tree G A → Formula
+| hnode a => a
+| @inode _ a b _ _ => a ⊃ b
+| @enode _ _ _ b t₁ t₂ => b
+
+theorem Tree.context_ok (t : Tree G A) : t.context = G := by
+  cases t <;> rfl
+
+theorem Tree.formula_ok (t : Tree G A) : t.formula = A := by
+  cases t <;> rfl
+
+-- Gets the leaf formulas of tree.
+def Tree.leaves : Tree G A → Context
+| hnode a => [a]
+| inode _ t _ => t.leaves
+| enode t₁ t₂ => t₁.leaves ++ t₂.leaves
+
+-- Gets the derivation (Prop) corresponding to tree.
+def Tree.deriv : Tree G A → Derivation G A
+| hnode a => hypo a
+| @inode g a b t h =>
+    by app impI t.deriv; apply (List.mem_of_elem_eq_true h)
+| @enode g₁ g₂ a b t₁ t₂ => by app impE t₁.deriv t₂.deriv
+
+section
+def t0 : Tree [0] 0 := hnode 0
+def t1 : Tree [] (0⊃0) := inode 0 t0 rfl
+def t2 : Tree [0, 0⊃1] 1 := enode t0 (Tree.hnode (0⊃1))
+def t3 : Tree [0, 0⊃1, 1⊃2] 2 := enode t2 (hnode (1⊃2))
+def t4 : Tree [0⊃1, 1⊃2] (0⊃2) := inode 0 t3 rfl
+
+example : t0.context == [0]             := by rfl
+example : t1.context == []              := by rfl
+example : t2.context == [0, 0⊃1]        := by rfl
+example : t3.context == [0, 0⊃1, 1⊃2]   := by rfl
+example : t4.context == [0⊃1, 1⊃2]      := by rfl
+
+example : t0.formula == 0               := by rfl
+example : t1.formula == 0⊃0             := by rfl
+example : t2.formula == 1               := by rfl
+example : t3.formula == 2               := by rfl
+example : t4.formula == 0⊃2             := by rfl
+
+example : t0.leaves == [0]              := by rfl
+example : t1.leaves == [0]              := by rfl
+example : t2.leaves == [0, 0⊃1]         := by rfl
+example : t3.leaves == [0, 0⊃1, 1⊃2]    := by rfl
+example : t4.leaves == [0, 0⊃1,1⊃2]     := by rfl
+
+#check (t0.deriv : [0] ⊢ 0)
+#check (t1.deriv : [] ⊢ 0⊃0)
+#check (t2.deriv : [0, 0⊃1] ⊢ 1)
+end
+
+theorem Tree.context_sub_leaves (t : Tree G A) :
+    t.context.Sublist t.leaves := by
+  sorry
 
 -- DLDS --------------------------------------------------------------------
+
+structure Node where
+  id      : Nat
+  formula : Formula
+  deriving BEq, ReflBEq, LawfulBEq, DecidableEq
+
+structure Edge where
+  orig    : Node
+  dest    : Node
+  deps    : Context
+  deriving BEq, ReflBEq, LawfulBEq, DecidableEq
+
+structure DLDS where
+  nodes : List Node
+  edges : List Edge
+  root : Node
+  rootP : root ∈ nodes ∧ forall e, e ∈ edges → e.orig != root
+
+def x : Node := {id := 0, formula := 0}
+def y : Node := {id := 1, formula := 1}
+
+#check ({
+  nodes := [x],
+  edges := [],
+  root  := x,
+  rootP := by simp
+} : DLDS)
+
+#check ({
+  nodes := [x, y],
+  edges := [{orig := x, dest := y, deps := []}],
+  root  := y,
+  rootP := by unfold x y; simp
+} : DLDS)
